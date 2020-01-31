81b50ce110154bd2d0db7ae2702417035ab4490a
doorjay <54376921+doorjay@users.noreply.github.com>
2020-01-31 12:55:09 -0800

    Create methods.rb

---
+++ methods.rb
@@ -0,0 +1,102 @@
+#Print me
+
+def print_me
+  puts "I'm printing with the method!"
+end 
+print_me  
+
+#Print me pt 2 
+
+def print_me 
+  "I'm printing with the return value!"
+end 
+
+puts print_me 
+
+#Greeting through methods
+
+def greeting 
+  "Hello "
+end 
+
+def place
+  "World!" 
+end 
+
+puts "#{greeting} #{place}" 
+
+#Greeting through methods pt 2 
+
+def hello
+  'Hello'
+end
+
+def world
+  'World'
+end
+
+def greet 
+  hello + ' ' + world 
+end 
+
+puts greet 
+
+#Make and model 
+
+def car(make, model) 
+  puts "#{make} #{model}" 
+end 
+
+car('Toyota', 'Corolla') 
+
+#Print me
+
+def print_me
+  puts "I'm printing with the method!"
+end 
+print_me  
+
+#Print me pt 2 
+
+def print_me 
+  "I'm printing with the return value!"
+end 
+
+puts print_me 
+
+#Greeting through methods
+
+def greeting 
+  "Hello "
+end 
+
+def place
+  "World!" 
+end 
+
+puts "#{greeting} #{place}" 
+
+#Greeting through methods pt 2 
+
+def hello
+  'Hello'
+end
+
+def world
+  'World'
+end
+
+def greet 
+  hello + ' ' + world 
+end 
+
+puts greet 
+
+#Make and model 
+
+def car(make, model) 
+  puts "#{make} #{model}" 
+end 
+
+car('Toyota', 'Corolla') 
+
